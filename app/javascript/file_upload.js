c4fb46c030c4148d9a5e266ae316b27f1e2533dc
flaviogf <flavio.fernandes6@gmail.com>
2021-07-03 10:06:56 -0300

    feat: add remove file
    Add feature that allow remove a file

--- app/javascript/file_upload.js
+++ app/javascript/file_upload.js
@@ -9,8 +9,6 @@ import {
 export function configureFileUploadFor(formGroup) {
   const dataField = document.querySelector('.form__field--data')
 
-  const fileField = document.querySelector('.form__field--file')
-
   const progressBar = document.querySelector('.form__progress-bar')
 
   const uppy = Core({
@@ -43,11 +41,53 @@ export function configureFileUploadFor(formGroup) {
     const imagePreview = document.querySelector('.form__image-preview')
 
     imagePreview.src = preview
+
+    const imagePreviewWrapper = document.querySelector('.form__image-preview-wrapper')
+
+    if (imagePreviewWrapper.classList.contains('form__image-preview-wrapper--with-image')) return
+
+    imagePreviewWrapper.classList.add('form__image-preview-wrapper--with-image')
+
+    configureRemoveFile()
   })
 
   uppy.on('upload-success', (file, response) => {
     const uploadedFileData = response.body['data']
 
     dataField.value = JSON.stringify(uploadedFileData)
+
+    formGroup.classList.add('hidden')
   })
+
+  configureRemoveFile()
+
+  function configureRemoveFile() {
+    const removeFileButton = document.querySelector('.form__image-preview-wrapper--with-image')
+  
+    if (!removeFileButton) return
+    
+    removeFileButton.addEventListener('click', () => {
+      uppy.getFiles().forEach(file => uppy.removeFile(file.id))
+
+      formGroup.classList.remove('hidden')
+
+      dataField.value = null
+  
+      let imagePreview = document.querySelector('.form__image-preview')
+  
+      imagePreview.remove()
+  
+      const imagePreviewWrapper = document.querySelector('.form__image-preview-wrapper')
+  
+      imagePreview = document.createElement('img')
+  
+      imagePreview.classList.add('form__image-preview')
+  
+      imagePreviewWrapper.appendChild(imagePreview)
+  
+      if (!imagePreviewWrapper.classList.contains('form__image-preview-wrapper--with-image')) return
+  
+      imagePreviewWrapper.classList.remove('form__image-preview-wrapper--with-image')
+    })
+  }
 }
